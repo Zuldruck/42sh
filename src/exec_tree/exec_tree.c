81e19501b02e0d172d7e92f867967b64bd632fc6
Lucas Sanchez <lucas.sanchez@epitech.eu>
2018-05-17 23:23:54 +0200

    static const tabs for built ins and operators

--- src/exec_tree/exec_tree.c
+++ src/exec_tree/exec_tree.c
@@ -7,14 +7,22 @@
 
 #include "42sh.h"
 
+static const operator_t op_tab[] = 
+{
+	{";", &exec_semicolon},
+	{"|", &exec_pipe},
+	{">", &exec_right_redirect},
+	{"<", &exec_left_redirect},
+	{">>", &exec_double_right_redirect},
+	{"<<", &exec_double_left_redirect},
+	{"&&", &exec_double_and},
+	{"||", &exec_double_or},
+	{NULL, NULL}
+};
+
 void exec_tree(btree_t *tree, env_t *env, int *ret_value)
 {
 	char **word_tab = NULL;
-	operator_t op_tab[] = {{";", &exec_semicolon},
-	{"|", &exec_pipe}, {">", &exec_right_redirect},
-	{"<", &exec_left_redirect}, {">>", &exec_double_right_redirect},
-	{"<<", &exec_double_left_redirect}, {"&&", &exec_double_and},
-	{"||", &exec_double_or}, {NULL, NULL}};
 
 	for (int i = 0; tree->op && op_tab[i].fptr; i++) {
 		if (!my_strcmp(tree->op, op_tab[i].op)) {
