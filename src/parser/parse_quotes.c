c958fe221ab5fb6cf0e0162d42db3ba8bd6a7197
Lucas Sanchez <lucas.sanchez@epitech.eu>
2018-05-21 16:13:17 +0200

    Beginning of quotes

--- src/parser/parse_quotes.c
+++ src/parser/parse_quotes.c
@@ -25,9 +25,38 @@ int check_quotes(char *cmd, int i)
 	return (-1);
 }
 
-// void parse_quotes(char **cmd)
-// {
-// 	for (int i = 0; cmd[i]; i++) {
+int str_contain_quotes(char *cmd)
+{
+	for (int i = 0; cmd[i]; i++)
+		if (cmd[i] == '"' || cmd[i] == '\'')
+			return (1);
+	return (0);
+}
 
-// 	}
-// }
+char *get_next_arg(char **cmd)
+{
+	int size = 0;
+	int word = 0;
+	char *arg = NULL;
+
+	if (!str_contain_quotes(cmd[word]))
+		return (cmd[word]);
+	for (int i = 0; cmd[word][i]; i++) {
+		arg = realloc(arg, size + 1);
+		arg[size] = cmd[word][i];
+		size += 1;
+	}
+	return (arg);
+}
+
+void parse_quotes(char **cmd)
+{
+	char *str = NULL;
+	char **cmd = malloc(sizeof(char *));
+
+	while ((str = get_next_arg(cmd))) {
+		if (!str_contain_char(cmd[0]))
+			cmd += 1;
+		printf("%s\n", str);
+	}
+}
