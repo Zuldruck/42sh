65bd75743b02ad9622a1efcc6e4e56c5270bfa16
Lucas Sanchez <lucas.sanchez@epitech.eu>
2018-05-22 10:31:45 +0200

    Issue on quotes \0 misplaced

--- src/parser/parse_quotes.c
+++ src/parser/parse_quotes.c
@@ -25,38 +25,48 @@ int check_quotes(char *cmd, int i)
 	return (-1);
 }
 
-int str_contain_quotes(char *cmd)
+char *get_next_arg(char ***cmd, char *arg, int *size, char *quote_char)
 {
-	for (int i = 0; cmd[i]; i++)
-		if (cmd[i] == '"' || cmd[i] == '\'')
-			return (1);
-	return (0);
-}
-
-char *get_next_arg(char **cmd)
-{
-	int size = 0;
-	int word = 0;
-	char *arg = NULL;
-
-	if (!str_contain_quotes(cmd[word]))
-		return (cmd[word]);
-	for (int i = 0; cmd[word][i]; i++) {
-		arg = realloc(arg, size + 1);
-		arg[size] = cmd[word][i];
-		size += 1;
+	if (!(*cmd)[0])
+		return (NULL);
+	printf("cmd[0] = %s\n", (*cmd)[0]);
+	for (int i = 0; (*cmd[0]) && (*cmd[0])[i]; i++) {
+		if ((*cmd[0])[i] == '"' || (*cmd[0])[i] == '\'') {
+			*quote_char = (*quote_char == 0 ? (*cmd[0])[i] : 0);
+			continue;
+		}
+		arg = realloc(arg, *size);
+		arg[*size - 1] = (*cmd[0])[i];
+		*size += 1;
 	}
+	*cmd += 1;
+	if ((*cmd)[0] && *quote_char) {
+		arg = realloc(arg, *size);
+		arg[*size - 1] = ' ';
+		*size += 1;
+		arg = get_next_arg(cmd, arg, size, quote_char);
+	}
+	arg = realloc(arg, *size);
+	arg[*size - 1] = 0;
 	return (arg);
 }
 
 void parse_quotes(char **cmd)
 {
 	char *str = NULL;
-	char **cmd = malloc(sizeof(char *));
+	int size = 1;
+	char quote_char = 0;
 
-	while ((str = get_next_arg(cmd))) {
-		if (!str_contain_char(cmd[0]))
-			cmd += 1;
+	while ((str = get_next_arg(&cmd, NULL, &size, &quote_char))) {
 		printf("%s\n", str);
+		free(str);
+		str = NULL;
 	}
 }
+
+// int main(int ac, char **av)
+// {
+
+// 	parse_quotes(av + 1);
+// 	return (0);
+// }
